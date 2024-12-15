067422ca9e9ed91cf1b0dea3e9df4e2d98c4db61
Martin Schere <tinschere@gmail.com>
2024-12-15 20:26:59 +0100

    chore: only run relay

--- .github/image/entrypoint.sh
+++ .github/image/entrypoint.sh
@@ -3,9 +3,6 @@
 echo "Starting dolos bootstrap"
 
 # Create empty WAL
-dolos bootstrap --config /etc/dolos.toml
-
-echo "Starting dolos daemon"
-
-# Now you can run
-dolos daemon --config /etc/dolos.toml
+dolos bootstrap relay --config /etc/dolos.toml && \
+  # Now you can run
+  dolos daemon --config /etc/dolos.toml
