9290dc5da072b7316816337d83b3661b917fdf7e
Martin Schere <tinschere@gmail.com>
2024-12-19 13:23:21 +0100

    chore: fix mithril

--- .github/image/entrypoint.sh
+++ .github/image/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Starting dolos bootstrap from snapshot"
 
-dolos bootstrap mithril --variant full --config /etc/dolos.toml
+dolos bootstrap mithril --skip-if-not-empty --config /etc/dolos.toml
 
 dolos daemon --config /etc/dolos.toml
